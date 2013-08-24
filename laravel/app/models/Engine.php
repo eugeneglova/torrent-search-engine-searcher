5447fb274a65edc0a6791b38f49ef55aab17adf6
Eugene Glova <eugene.glova@gmail.com>
2013-08-24 23:09:48 +0300

    list visible attributes for engine model

--- laravel/app/models/Engine.php
+++ laravel/app/models/Engine.php
@@ -4,4 +4,11 @@ class Engine extends Eloquent {
 
     protected $table = 'ss2_sites';
 
+    protected $visible = array(
+        'id',
+        'name',
+        'home_url',
+        'search_url'
+    );
+
 }
