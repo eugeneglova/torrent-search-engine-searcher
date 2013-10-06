36ff021d90a95370a2abe552349b2de6c5c95e81
Eugene Glova <eugene.glova@gmail.com>
2013-10-06 11:24:08 +0300

    refactor method name

--- dev/scripts/modules/data/pages/index.js
+++ dev/scripts/modules/data/pages/index.js
@@ -21,7 +21,7 @@ define([
             // Initialize pages collection
             this.pages = new PagesCollection();
 
-            this.listenTo(this.pages, 'reset', this.onPagesReset, this);
+            this.listenTo(this.pages, 'reset', this.onReset, this);
 
             this.pages.fetch({ reset: true });
 
@@ -34,7 +34,7 @@ define([
             return true;
         },
 
-        onPagesReset: function() {
+        onReset: function() {
             this.announce('ready');
 
             return true;
